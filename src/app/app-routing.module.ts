4bb89ccc65f35aa9e26756ad7e5f1c17f8170f91
geisonfeitosa <geisonlealfeitosa.glf@gmail.com>
2020-05-23 18:31:36 -0300

    alter color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'menu/Fleets',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'menu/:id',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
 ];
 
