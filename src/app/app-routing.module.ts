0aa51b1017c8442e4a1d964fc9b007214ecac3f0
geisonfeitosa <geisonlealfeitosa.glf@gmail.com>
2020-05-24 02:20:09 -0300

    tela login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { SolicitacaoTransporteComponent } from './home/solicitacao-transporte/solicitacao-transporte.component';
 import { HomePage } from './home/home.page';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
